086e113ff75f2f6026b58f7a75d0ef0847cd112f
valera <valery.sig1@gmail.com>
2019-04-27 14:15:19 +0300

    Implemented CardsServiceImpl

--- app/src/main/java/com/valera/speakhumanish/services/CardsServiceImpl.kt
+++ app/src/main/java/com/valera/speakhumanish/services/CardsServiceImpl.kt
@@ -1,27 +1,51 @@
 package com.valera.speakhumanish.services
 
-import android.util.Log
 import com.valera.speakhumanish.model.Card
+import com.valera.speakhumanish.model.Supplier
 import javax.inject.Inject
 
 class CardsServiceImpl
     @Inject constructor() : ICardsService {
 
+    //region Members
+    private val allCards : Map<Long, Card>
+    private val staticCards : List<Card>
+    private var mainCards : List<Card>
+    private var pressedCards : MutableList<Card>
+    private var previousCardsStack : MutableList<List<Card>>
+    //endregion
+
+    init {
+        //TODO: After the JSON parsing class is done remove the following 3 lines
+        allCards = Supplier.allCards.map { it.id to it }.toMap()
+        staticCards = Supplier.StaticCards
+        mainCards = Supplier.mainCards
+        pressedCards = mutableListOf()
+        previousCardsStack = mutableListOf()
+    }
 
     override fun getStaticCards(): List<Card> {
-        Log.d("CardsServiceImpl", "getStaticCards")
-        return listOf()
+        return this.staticCards
     }
 
     override fun getMainCards(): List<Card> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return this.mainCards
     }
 
     override fun updatePresentingCards(selectedCard: Card) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        pressedCards.add(selectedCard)
+        previousCardsStack.add(this.mainCards)
+
+        val newCardsIndicies = selectedCard.possibleChildren
+        this.mainCards = allCards.filter { newCardsIndicies.contains(it.key) }.values.toList()
     }
 
     override fun backOneLevel() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (previousCardsStack.isNotEmpty()) {
+            this.mainCards = previousCardsStack.last()
+            previousCardsStack.dropLast(1)
+
+            pressedCards.dropLast(1)
+        }
     }
 }
