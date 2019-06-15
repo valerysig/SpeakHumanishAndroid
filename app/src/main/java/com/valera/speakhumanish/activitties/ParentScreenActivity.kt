61cfba5d7fe043a837c9cd39953281c95c35ef13
valera <valery.sig1@gmail.com>
2019-06-15 22:00:23 +0300

    [Add a Parent Screen] Added the parent fragments selection option

--- app/src/main/java/com/valera/speakhumanish/activitties/ParentScreenActivity.kt
+++ app/src/main/java/com/valera/speakhumanish/activitties/ParentScreenActivity.kt
@@ -1,13 +1,46 @@
 package com.valera.speakhumanish.activitties
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MenuItem
 import com.valera.speakhumanish.R
 
-class ParentScreenActivity : Activity() {
+class ParentScreenActivity : AppCompatActivity() {
+    private val staticCardsFragment = ParentScreenStaticCardsFragment()
+    private val mainCardsFragment = ParentScreenMainCardsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Android init
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parent_screen)
+
+        // Init the fragments selection functionality
+        val navigationView : BottomNavigationView = findViewById(R.id.parent_screen_navigation)
+        navigationView.setOnNavigationItemSelectedListener { menuItem -> getNavListener(menuItem) }
+
+        // Init the default fragment
+        supportFragmentManager.beginTransaction().replace(R.id.parent_screen_fragment_container, staticCardsFragment)
+            .commit()
+    }
+
+    private fun getNavListener(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_main_cards_editor -> {
+                supportFragmentManager.beginTransaction().replace(R.id.parent_screen_fragment_container, mainCardsFragment)
+                    .commit()
+                return true
+            }
+
+            R.id.nav_static_cards_editor -> {
+                supportFragmentManager.beginTransaction().replace(R.id.parent_screen_fragment_container, staticCardsFragment)
+                    .commit()
+                return true
+            }
+        }
+
+        Log.e("ParentScreenActivity", "Something wrong with selected a fragment")
+        return false
     }
 }
