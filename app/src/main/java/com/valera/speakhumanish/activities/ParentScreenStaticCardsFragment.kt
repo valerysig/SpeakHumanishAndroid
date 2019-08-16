e0d47ac17c6f2500e68c48e7ff2982874ad67134
valera <valery.sig1@gmail.com>
2019-08-17 01:17:54 +0300

    [Parent screen] Inject services into the parent fragments

--- app/src/main/java/com/valera/speakhumanish/activities/ParentScreenStaticCardsFragment.kt
+++ app/src/main/java/com/valera/speakhumanish/activities/ParentScreenStaticCardsFragment.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.valera.speakhumanish.R
+import com.valera.speakhumanish.services.ICardsService
+import javax.inject.Inject
 
-class ParentScreenStaticCardsFragment : androidx.fragment.app.Fragment() {
+class ParentScreenStaticCardsFragment : Fragment() {
+
+    @Inject
+    lateinit var cardsService: ICardsService
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_parent_screen_static_cards, container, false)
     }
